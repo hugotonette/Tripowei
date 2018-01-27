0a802c4a33cac0cf5e419d6d06a0b15dff971fad
Eduardo de Albuquerque <erizzoalbuquerque@gmail.com>
2018-01-27 02:57:21 -0200

    Atualizei o movimento

--- Project Lamarque/Assets/Script/PlayerMovement.cs
+++ Project Lamarque/Assets/Script/PlayerMovement.cs
@@ -4,60 +4,98 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public float MaxSpeedX, MaxSpeedY;
+    public float GroundAccel, AirAccel;
+    public float JumpSpeed;
 
-    public float Speed = 10f;
-    public float JumpForce = 400f;
-    public bool CanMoveOnAir = true;
+    public Collider2D groundCheckCol;
+    public LayerMask whatIsSolid;
 
     private Rigidbody2D _playerBody;
-    private bool _canJump;
+    private bool _onGround;
 
+    float _axisHorizontal;
+    bool _kJumpDown, _kJumpRelease;
 
     private void Awake()
     {
         _playerBody = GetComponent<Rigidbody2D>();
+
+        _kJumpDown = false;
+        _kJumpRelease = false;
+    }
+
+    private void Update()
+    {
+        // Checking Inputs. As the inputs are updated only on the Update, we only check them here.
+        _axisHorizontal = Input.GetAxis("Horizontal");
+
+        _kJumpDown = Input.GetButtonDown("Jump");
+        _kJumpRelease = Input.GetButtonUp("Jump");
+
+        _onGround = Physics2D.IsTouchingLayers(groundCheckCol, whatIsSolid);
+
+        // As our movement don't apply forces, we can apply it during the Update. 
+        // Applying the movement during the Fixed Update can result in losing Inputs.
+        ApplyMovement();
     }
 
     private void FixedUpdate()
     {
-        if (_canJump)
+        //Nothing...
+    }
+
+
+    void ApplyMovement()
+    {
+        float currentSpeedY = _playerBody.velocity.y;
+
+        float targetSpeedX;
+        float currentSpeedX;
+        currentSpeedX = _playerBody.velocity.x;
+        targetSpeedX = MaxSpeedX * _axisHorizontal;
+
+        if (_onGround)
+            currentSpeedX = Common.ApproachUniform(targetSpeedX, currentSpeedX, GroundAccel);
+        else
+            currentSpeedX = Common.ApproachUniform(targetSpeedX, currentSpeedX, AirAccel);
+
+        if (currentSpeedY > MaxSpeedY)
+            currentSpeedY = Common.ApproachUniform(MaxSpeedY, currentSpeedY, AirAccel);
+        else if (currentSpeedY < -MaxSpeedY)
+            currentSpeedY = Common.ApproachUniform(-MaxSpeedY, currentSpeedY, AirAccel);
+
+        if (_onGround)
         {
-            if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.UpArrow))  //Jump
+            if (_kJumpDown)  //Jump
             {
                 Debug.Log("jump");
-                _playerBody.AddForce(new Vector2(0f, JumpForce));
+                currentSpeedY += JumpSpeed;
             }
         }
 
-        //Linear Drag (no RigidBody2D define o atrito (tempo q demora pra parar o movimento)
-        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))  //Right
+        if (_playerBody.velocity.y > 0.0f && _kJumpRelease)
         {
-            Debug.Log("Right");
-            _playerBody.velocity = new Vector2(Speed, _playerBody.velocity.y);
-        }
-        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))  //Left
-        {
-            Debug.Log("Left");
-            _playerBody.velocity = new Vector2(-Speed, _playerBody.velocity.y);
+            currentSpeedY *= 0.25f;
         }
 
-
+        _playerBody.velocity = new Vector2(currentSpeedX, currentSpeedY);
     }
 
-    private void OnCollisionEnter2D(Collision2D col)
-    {
-        if (col.gameObject.CompareTag("Floor"))  //Está encostando no chão
-        {
-            _canJump = true;
-        }
-    }
-
-    private void OnCollisionExit2D(Collision2D col)
-    {
-        if (col.gameObject.CompareTag("Floor"))
-        {
-            _canJump = false;
-        }
-    }
+//    private void OnCollisionEnter2D(Collision2D col)
+//    {
+//        if (col.gameObject.CompareTag("Floor"))  //Está encostando no chão
+//        {
+//            _onGround = true;
+//        }
+//    }
+//
+//    private void OnCollisionExit2D(Collision2D col)
+//    {
+//        if (col.gameObject.CompareTag("Floor"))
+//        {
+//            _onGround = false;
+//        }
+//    }
 
 }
