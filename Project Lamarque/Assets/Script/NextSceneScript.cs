70a7b948c52ec10d1c29b74d620d0fe8801016fd
Hugo Tonette <hugo.tonette@gmail.com>
2018-01-27 15:53:41 -0200

    NextSceneScript

---
+++ Project Lamarque/Assets/Script/NextSceneScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextSceneScript : MonoBehaviour
+{
+    public string NextScene;
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.CompareTag("Player"))
+            if (NextScene.Length == 0)
+                Debug.Log("Insert Scene Here");
+            else
+                SceneManager.LoadScene(NextScene);
+    }
+}
