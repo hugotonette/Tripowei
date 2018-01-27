2a6e1e6729fb5342b0455baab8c5c3ffe3ad4d7f
Eduardo de Albuquerque <erizzoalbuquerque@gmail.com>
2018-01-27 16:09:45 -0200

    Refactory grab + Throw

---
+++ Project Lamarque/Assets/GrabSkill.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrabSkill : MonoBehaviour {
+
+    public float MinSpeedToThrow = 0.1f;
+    public float ThrowForce = 10f;
+
+    List<Grabbable> _itensOnReach;
+    Grabbable _itemBeingHeld;
+
+    Rigidbody2D _playerRb; // Para calcular arremesso.
+
+	// Use this for initialization
+	void Start () {
+        _itensOnReach = new List<Grabbable>();
+
+        _playerRb = GetComponent<Rigidbody2D>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetButtonDown("Grab") && CanHold())
+        {
+            if (_itensOnReach.Count > 0)
+                GrabItem(_itensOnReach[0]);        
+        }		
+        if (Input.GetButtonUp("Grab") && _itemBeingHeld != null)
+        {
+            ReleaseItem();
+        }
+	}
+
+
+    void GrabItem(Grabbable item)
+    {
+        _itemBeingHeld = item;
+        _itemBeingHeld.Hold(this);
+    }
+
+
+    public void ReleaseItem(bool enableThrow = true)
+    {
+        if (_itemBeingHeld != null) // e sempre deveria ser...
+        {
+            _itemBeingHeld.Release();
+
+            //Throw
+            if (Mathf.Abs(_playerRb.velocity.x) > MinSpeedToThrow && enableThrow == true)
+            {
+                _itemBeingHeld.GetRigidBody2d().velocity = new Vector2(1f * Mathf.Sign(_playerRb.velocity.x), 1f).normalized * ThrowForce;
+            }
+
+            _itemBeingHeld = null;
+        }
+    }
+
+
+    bool CanHold()
+    {
+        if (_itemBeingHeld != null)
+            return false;
+        else
+            return true;
+    }
+
+    private void OnDestroy()
+    {
+        ReleaseItem(false);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Item"))
+        {
+            Grabbable item = collision.GetComponent<Grabbable>();
+            if (!_itensOnReach.Contains(item))
+                _itensOnReach.Add(collision.GetComponent<Grabbable>());
+        }
+    }
+
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Item"))
+        {
+            _itensOnReach.Remove(collision.GetComponent<Grabbable>());
+        }
+    }
+}
---
+++ Project Lamarque/Assets/Grabbable.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grabbable : MonoBehaviour
+{
+
+    public float PlayerXOffset;
+    public float PlayerYOffset;
+
+    Rigidbody2D _rb;
+    GrabSkill _holder ;
+
+    Vector2 _startPos;
+    float _startRot;
+
+    // Use this for initialization
+    void Start()
+    {
+        _rb = this.GetComponent<Rigidbody2D>();
+
+        _startPos = this.transform.position;
+        _startRot = _rb.rotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (IsHeld())
+        {
+            this.transform.position = _holder.transform.position + new Vector3(PlayerXOffset, PlayerYOffset,0.0f);
+        }
+    }
+
+    public void Hold(GrabSkill holder)
+    {
+        print(gameObject.name + " was held");
+        _rb.simulated = false;
+
+        _holder = holder;
+    }
+
+    public Rigidbody2D GetRigidBody2d()
+    {
+        return _rb;
+    }
+
+    public void Release()
+    {
+        print(gameObject.name + " was released");
+        _rb.simulated = true;
+
+        _holder = null;
+    }
+
+    public bool IsHeld()
+    {
+        if (_holder != null)
+            return true;
+        else
+            return false;
+    }
+
+    public void Respawn() // Não vai ser usado, por enquanto.
+    {
+        if (_holder != null)
+        {
+            _holder.ReleaseItem(false);
+        }
+
+        _rb.velocity = Vector2.zero;
+
+        _rb.MovePosition(_startPos);
+        _rb.MoveRotation(_startRot);
+    }
+}
