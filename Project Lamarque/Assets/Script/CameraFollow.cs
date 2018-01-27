ca1105efd98f2391366c9486b25d36469638df70
DESKTOP-F6E7B82\behor <be.horner@hotmail.com>
2018-01-27 13:01:12 -0200

    Proto level design, criação de script de câmera que segue o player e mudanças de cores.

---
+++ Project Lamarque/Assets/Script/CameraFollow.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    private GameObject player;
+    public Vector3 offset;
+    private bool reSnap = true;
+    void Update() {
+        if (reSnap)
+        {
+            player = GameObject.FindGameObjectWithTag("Player");
+            reSnap = false;
+        }
+        if (!GameObject.FindGameObjectWithTag("Player"))
+        {
+            reSnap = true;
+        }
+        transform.position = player.transform.position + offset;
+	}
+}
